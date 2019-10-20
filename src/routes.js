303bfc8563944f9b30996d34e45d781a815a6d4b
Marcos De Souza <m90k@MacBook-Air-de-Marcos.local>
2019-10-20 13:28:33 -0300

    Validando os dados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
-import {Router} from 'express';
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import LoginController from './app/controllers/LoginController';
+
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (res, req) => {
-  return res.json({message: ' new Router '});
-});
+routes.post('/users', UserController.store);
+routes.post('/login', LoginController.store);
+
+routes.use(auth);
+
+routes.put('/users', UserController.update);
 
 export default routes;
